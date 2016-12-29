df01ee623640866fe977e675deadca5dfe0c4a98
SelenaSmall <selenawiththetattoo@gmail.com>
2016-12-29 19:44:24 +1300

    seed admin user

--- db/migrate/20161226041506_create_seed_user.rb
+++ db/migrate/20161226041506_create_seed_user.rb
@@ -1,6 +1,11 @@
 class CreateSeedUser < ActiveRecord::Migration[5.0]
-  def change
-    create_table :seed_users do |t|
-    end
+  def up
+    User.create(email: 'selenawiththetattoo@gmail.com',
+    			role: 'admin',
+    			password: 'password')
+  end
+
+  def down
+  	User.where(email: 'selenawiththetattoo@gmail.com').destroy_all
   end
 end
